ef25c470597316497ea917cf206aec2912e4737a
sndpkumar14 <sndp.kumar@gmail.com>
2014-05-21 10:41:51 -0400

    Update cachematrix.R
    version 0 of function strawman

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## It returns matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- matrix(m) m <<- x
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+
 }
+## end of function makeCacheMatrix
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## It takes in matrix X as the parameter and returns its inverse.
+## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+## It is assumed that inputs matrix is a square invertible matrix for this assignment.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve  retrieves the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        cachematrix <- function(x, ...) {
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- matrix(data, ...)
+        x$setmatrix(m)
+        m
 }
